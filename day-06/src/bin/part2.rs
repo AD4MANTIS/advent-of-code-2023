9f5165c3e9c76499f3bc6608bf36dc51c1301029
AD4MANTIS <31745687+AD4MANTIS@users.noreply.github.com>
2023-12-06 20:34:26 +0100

    day-06 part2  done

--- day-06/src/bin/part2.rs
+++ day-06/src/bin/part2.rs
@@ -14,34 +14,28 @@ fn part1(input: &str) -> usize {
         .map(|line| {
             line.split(' ')
                 .flat_map(str::parse::<u32>)
-                .collect::<Vec<_>>()
+                .fold("".to_owned(), |acc, num| acc + &num.to_string())
+                .parse::<usize>()
+                .unwrap()
         })
         .collect::<Vec<_>>();
 
-    let games = lines[0].iter().zip(lines[1].iter()).collect::<Vec<_>>();
+    let game = (lines[0], lines[1]);
 
-    let mut number_of_ways_you_can_beat_the_record_per_game = Vec::with_capacity(games.len());
+    let mut number_of_ways_you_can_beat_the_record = 0;
 
-    for game in games {
-        let time = *game.0;
-        let best_distance = *game.1;
+    let time = game.0;
+    let best_distance = game.1;
 
-        number_of_ways_you_can_beat_the_record_per_game.push(0);
+    for hold_time in 1..time {
+        let speed = hold_time;
 
-        for hold_time in 1..time {
-            let speed = hold_time;
-
-            if (time - hold_time) * speed > best_distance {
-                *number_of_ways_you_can_beat_the_record_per_game
-                    .last_mut()
-                    .unwrap() += 1;
-            }
+        if (time - hold_time) * speed > best_distance {
+            number_of_ways_you_can_beat_the_record += 1;
         }
     }
 
-    number_of_ways_you_can_beat_the_record_per_game
-        .iter()
-        .product()
+    number_of_ways_you_can_beat_the_record
 }
 
 #[cfg(test)]
@@ -51,6 +45,6 @@ mod tests {
     #[test]
     fn it_works() {
         let result = part1(include_str!("./test-input.txt"));
-        assert_eq!(result, 288);
+        assert_eq!(result, 71503);
     }
 }
