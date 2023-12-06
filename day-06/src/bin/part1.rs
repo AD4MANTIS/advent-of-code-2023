49a4d2252f870a5d6b80378b23802274a8a071fb
AD4MANTIS <31745687+AD4MANTIS@users.noreply.github.com>
2023-12-06 20:17:12 +0100

    day-06 part1

--- day-06/src/bin/part1.rs
+++ day-06/src/bin/part1.rs
@@ -9,7 +9,39 @@ fn main() {
 
 #[allow(unused_variables)]
 fn part1(input: &str) -> usize {
-    todo!()
+    let lines = input
+        .lines()
+        .map(|line| {
+            line.split(' ')
+                .flat_map(str::parse::<u32>)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let games = lines[0].iter().zip(lines[1].iter()).collect::<Vec<_>>();
+
+    let mut number_of_ways_you_can_beat_the_record_per_game = Vec::with_capacity(games.len());
+
+    for game in games {
+        let time = *game.0;
+        let best_distance = *game.1;
+
+        number_of_ways_you_can_beat_the_record_per_game.push(0);
+
+        for hold_time in 1..time {
+            let speed = hold_time;
+
+            if (time - hold_time) * speed > best_distance {
+                *number_of_ways_you_can_beat_the_record_per_game
+                    .last_mut()
+                    .unwrap() += 1;
+            }
+        }
+    }
+
+    number_of_ways_you_can_beat_the_record_per_game
+        .iter()
+        .product()
 }
 
 #[cfg(test)]
@@ -19,6 +51,6 @@ mod tests {
     #[test]
     fn it_works() {
         let result = part1(include_str!("./test-input.txt"));
-        assert_eq!(result, 4);
+        assert_eq!(result, 288);
     }
 }
