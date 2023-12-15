166dd08d511e380232c6a7a94fab178535f6baa8
AD4MANTIS <31745687+AD4MANTIS@users.noreply.github.com>
2023-12-15 21:29:52 +0100

    day-15 part2 done

--- day-15/src/bin/part2.rs
+++ day-15/src/bin/part2.rs
@@ -1,7 +1,81 @@
-lib::day!(15, part1, example => 145);
+use std::{collections::HashMap, hash::Hash};
 
-fn part1(input: &str) -> u64 {
-    input.split(',').map(hash).sum()
+lib::day!(15, part2, example => 145, answer => 303404);
+
+fn part2(input: &str) -> u64 {
+    let mut boxes: HashMap<u64, Vec<Lens>> = (0..256).map(|id| (id, Vec::new())).collect();
+
+    for operation in input.split(',') {
+        let lens = Lens::from_string(operation);
+
+        boxes.entry(hash(&lens.label)).and_modify(|b| {
+            let pos = b
+                .iter()
+                .position(|lens_in_box| lens_in_box.label == lens.label);
+
+            match lens.operation {
+                '-' => {
+                    if let Some(pos) = pos {
+                        b.remove(pos);
+                    }
+                }
+                '=' => {
+                    if let Some(pos) = pos {
+                        b[pos] = lens;
+                    } else {
+                        b.push(lens);
+                    }
+                }
+                _ => panic!(),
+            };
+        });
+    }
+
+    boxes
+        .iter()
+        .filter(|x| !x.1.is_empty())
+        .flat_map(|lens_box| {
+            lens_box
+                .1
+                .iter()
+                .enumerate()
+                .map(move |lens| (lens_box.0 + 1) * (lens.0 as u64 + 1) * lens.1.focal_length)
+        })
+        .sum()
+}
+
+#[derive(Debug)]
+struct Lens {
+    label: String,
+    operation: char,
+    focal_length: u64,
+}
+
+impl Lens {
+    pub fn from_string(val: &str) -> Self {
+        let label = val
+            .chars()
+            .take_while(|c| *c >= 'a' && *c <= 'z')
+            .collect::<String>();
+
+        Self {
+            operation: val.chars().nth(label.len()).unwrap(),
+            label,
+            focal_length: val
+                .split('=')
+                .last()
+                .and_then(|focal_length| str::parse(focal_length).ok())
+                .unwrap_or_default(),
+        }
+    }
+}
+
+struct Label<'a>(&'a str);
+
+impl<'a> Hash for Label<'a> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        state.write_u64(hash(self.0));
+    }
 }
 
 fn hash(val: &str) -> u64 {
