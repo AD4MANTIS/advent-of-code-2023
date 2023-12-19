0a6330ba1897665a0c865839165a9d31447a6265
AD4MANTIS <31745687+AD4MANTIS@users.noreply.github.com>
2023-12-19 22:43:13 +0100

    day-19 part 2 try

--- day-19/src/bin/part2.rs
+++ day-19/src/bin/part2.rs
@@ -1,59 +1,46 @@
-use std::{collections::HashMap, str::FromStr};
+#![allow(unused)]
 
-lib::day!(19, part1, example => 19114, answer => 480738);
+use std::{iter, str::FromStr};
+
+// lib::day!(19, part2, example => 167409079868000);
+
+fn main() {}
 
 // px{a<2006:qkq,m>2090:A,rfg}
 #[derive(Debug, Clone)]
 pub struct Workflow {
-    name: String,
+    name: u32,
     flows: Vec<Flow>,
+    next_default_workflow: u32,
 }
 
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone)]
 pub struct Flow {
-    condition: Option<FlowCondition>,
-    next_workflow: String,
+    condition: FlowCondition,
+    next_workflow: u32,
 }
 
 #[derive(Debug, Clone)]
 pub struct FlowCondition {
-    category: PartCategory,
-    op: String,
-    rhs: usize,
+    /// See [PartCategory]
+    category: u8,
+    op: Operation,
+    rhs: u16,
 }
 
-#[derive(Debug, Clone)]
-pub struct Rating {
-    x: usize,
-    m: usize,
-    a: usize,
-    s: usize,
+#[derive(Debug, Clone, Copy)]
+pub enum Operation {
+    Less,
+    Greater,
 }
 
-impl Rating {
-    pub const fn get(&self, cat: PartCategory) -> usize {
-        match cat {
-            PartCategory::X => self.x,
-            PartCategory::M => self.m,
-            PartCategory::A => self.a,
-            PartCategory::S => self.s,
-        }
-    }
-
-    pub fn check(&self, cond: &FlowCondition) -> bool {
-        let value = self.get(cond.category);
-
-        match cond.op.as_str() {
-            ">" => value > cond.rhs,
-            "<" => value < cond.rhs,
-            _ => panic!(),
-        }
-    }
-
-    pub const fn sum(&self) -> usize {
-        self.x + self.m + self.a + self.s
-    }
-}
+// #[derive(Debug, Clone)]
+// pub struct Rating {
+//     x: u16,
+//     m: u16,
+//     a: u16,
+//     s: u16,
+// }
 
 #[derive(Debug, Clone, Copy)]
 pub enum PartCategory {
@@ -77,101 +64,123 @@ impl FromStr for PartCategory {
     }
 }
 
-fn part1(input: &'static str) -> usize {
-    let [workflows, ratings]: [&'static str; 2] =
+fn part2(input: &'static str) -> usize {
+    let [workflows, _ratings]: [&'static str; 2] =
         input.split("\n\n").collect::<Vec<_>>().try_into().unwrap();
 
     let workflows = get_workflows(workflows);
 
-    get_ratings(ratings)
-        .into_iter()
-        .filter(|rating| {
-            let mut workflow = workflows.get("in").expect("start should be present");
+    let mut count = 0_usize;
+    const IN_KEY: u32 = 28265; // hash("in");
+    const A_KEY: u32 = 65; //hash("A");
+    const R_KEY: u32 = 82; //hash("R");
 
-            while !matches!(workflow.name.as_str(), "A" | "R") {
-                for flow in workflow.flows.iter() {
-                    if let Some(cond) = &flow.condition {
-                        if !rating.check(cond) {
-                            continue;
+    for x in 1..=4000 {
+        dbg!(x);
+        for m in 1..=4000 {
+            dbg!(m);
+
+            for a in 1..=4000 {
+                for s in 1..=4000 {
+                    let rating = [x, m, a, s];
+
+                    let mut workflow = workflows[IN_KEY as usize].as_ref().unwrap();
+
+                    'workflow_loop: while workflow.name != A_KEY && workflow.name != R_KEY {
+                        for flow in workflow.flows.iter() {
+                            let value = rating[flow.condition.category as usize];
+
+                            if match flow.condition.op {
+                                Operation::Greater => value > flow.condition.rhs,
+                                Operation::Less => value < flow.condition.rhs,
+                            } {
+                                workflow = workflows[flow.next_workflow as usize].as_ref().unwrap();
+                                break 'workflow_loop;
+                            }
                         }
+
+                        // if workflow.next_default_workflow == a_key {
+                        //     count += 1;
+                        // }
+
+                        workflow = workflows[workflow.next_default_workflow as usize]
+                            .as_ref()
+                            .unwrap();
                     }
 
-                    match workflows.get(&flow.next_workflow) {
-                        Some(next_workflow) => workflow = next_workflow,
-                        None => return flow.next_workflow == "A",
+                    if workflow.name == A_KEY {
+                        count += 1;
                     }
-
-                    break;
                 }
             }
+        }
+    }
 
-            workflow.name == "A"
-        })
-        .map(|r| r.sum())
-        .sum()
-
-    // dbg!(&workflows);
-    // dbg!(&ratings);
+    count
 }
 
-fn get_ratings(ratings: &str) -> Vec<Rating> {
-    let ratings = ratings
-        .lines()
-        .map(|rating_str| {
-            let ratings_str: [usize; 4] = rating_str
-                .split(',')
-                .flat_map(|rating| {
-                    rating
-                        .chars()
-                        .filter(|c| char::is_numeric(*c))
-                        .collect::<String>()
-                        .parse::<usize>()
-                })
-                .collect::<Vec<_>>()
-                .try_into()
-                .unwrap();
-
-            Rating {
-                x: ratings_str[0],
-                m: ratings_str[1],
-                a: ratings_str[2],
-                s: ratings_str[3],
-            }
-        })
-        .collect::<Vec<_>>();
-    ratings
+fn hash(name: &str) -> u32 {
+    let mut chars = name.chars();
+    (chars.next().unwrap() as u8 as u32)
+        + ((chars.next().unwrap_or_default() as u8 as u32) << 8)
+        + ((chars.next().unwrap_or_default() as u8 as u32) << 16)
 }
 
-fn get_workflows(workflows: &str) -> HashMap<String, Workflow> {
-    let workflows = workflows
+fn get_workflows(workflows: &str) -> Vec<Option<Workflow>> {
+    let mut arr: Vec<Option<Workflow>> = iter::repeat(None::<Workflow>).take(8024434).collect();
+
+    workflows
         .lines()
         .map(|line| {
             let name = line.chars().take_while(|c| *c != '{').collect::<String>();
 
-            Workflow {
-                flows: line
-                    .trim_end_matches('}')
-                    .chars()
-                    .skip(name.len() + 1)
-                    .collect::<String>()
-                    .split(',')
-                    .map(|flow_str| {
-                        let mut flow_str = flow_str.split(':').rev();
+            let mut next_default_workflow = 0;
 
-                        Flow {
-                            next_workflow: flow_str.next().unwrap().to_string(),
-                            condition: flow_str.next().map(|cond| FlowCondition {
-                                category: PartCategory::from_str(&cond[0..=0]).unwrap(),
-                                op: cond[1..=1].to_string(),
-                                rhs: cond[2..].parse().unwrap(),
-                            }),
-                        }
-                    })
-                    .collect(),
-                name,
+            let flows = line
+                .trim_end_matches('}')
+                .chars()
+                .skip(name.len() + 1)
+                .collect::<String>()
+                .split(',')
+                .flat_map(|flow_str| {
+                    let mut flow_str = flow_str.split(':').rev();
+
+                    let next_workflow = hash(flow_str.next().unwrap());
+
+                    if let Some(cond) = flow_str.next().map(|cond| FlowCondition {
+                        category: match &cond[0..=0] {
+                            "x" => 0,
+                            "m" => 1,
+                            "a" => 2,
+                            "s" => 3,
+                            _ => panic!(),
+                        },
+                        op: match &cond[1..=1] {
+                            "<" => Operation::Less,
+                            ">" => Operation::Greater,
+                            _ => panic!(),
+                        },
+                        rhs: cond[2..].parse().unwrap(),
+                    }) {
+                        return Some(Flow {
+                            next_workflow,
+                            condition: cond,
+                        });
+                    }
+
+                    next_default_workflow = next_workflow;
+
+                    None
+                })
+                .collect();
+
+            Workflow {
+                name: hash(&name),
+                flows,
+                next_default_workflow,
             }
         })
-        .map(|workflow| (workflow.name.clone(), workflow))
-        .collect::<HashMap<_, _>>();
-    workflows
+        .for_each(|workflow| arr[workflow.name as usize] = Some(workflow.clone()));
+
+    arr
 }
